f8ceb3a6a35812e4e3055d08683e36b294ea43fc
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-04-15 16:54:14 -0400

    Adding colors to charts

--- src/app/grand-total-line-chart/grand-total-line-chart.component.ts
+++ src/app/grand-total-line-chart/grand-total-line-chart.component.ts
@@ -8,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class GrandTotalLineChartComponent {
   // lineChart
   public lineChartData: Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Loan'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Lease'},
   ];
   public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: any = {
@@ -18,44 +17,25 @@ export class GrandTotalLineChartComponent {
     maintainAspectRatio: false
   };
   public lineChartColors: Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    { // loan
+      backgroundColor: 'rgba(255, 133, 173, 0.2)',
+      borderColor: 'rgba(255, 133, 173, 1)',
+      pointBackgroundColor: '#fff',
+      pointBorderColor: '#FF85AE',
+      pointHoverBackgroundColor: 'rgba(255, 133, 173, 0.2)',
+      pointHoverBorderColor: 'rgba(255, 133, 173, 1)'
     },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    { // lease
+      backgroundColor: 'rgba(140, 151, 210, 0.2)',
+      borderColor: 'rgba(140, 151, 210, 1)',
+      pointBackgroundColor: '#fff',
+      pointBorderColor: 'rgba(140, 151, 210, 1)',
+      pointHoverBackgroundColor: 'rgba(140, 151, 210, 0.2)',
+      pointHoverBorderColor: 'rgba(140, 151, 210, 1)'
     }
   ];
   public lineChartLegend = true;
   public lineChartType = 'line';
-
-  public randomize(): void {
-    const _lineChartData: Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
 
   // events
   public chartClicked(e: any): void {
